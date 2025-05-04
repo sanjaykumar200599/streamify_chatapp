36d67bbb9d4b846816e74ef8c37925fd980ea957
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-04 22:23:58 +0530

    get friend request end point done

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,14 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { acceptFriendRequest, getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
-// import {
-//   acceptFriendRequest,
-//   getFriendRequests,
-//   getMyFriends,
-//   getOutgoingFriendReqs,
-//   getRecommendedUsers,
-//   sendFriendRequest,
-// } from "../controllers/user.controller.js";
+import { acceptFriendRequest, getFriendRequests, getMyFriends, getOutgoingFriendReqs, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -21,7 +13,7 @@ router.get("/friends", getMyFriends);
 router.post("/friend-request/:id", sendFriendRequest);
 router.put("/friend-request/:id/accept", acceptFriendRequest);
 
-// router.get("/friend-requests", getFriendRequests);
-// router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router;
