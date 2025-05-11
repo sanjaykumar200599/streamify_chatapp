d796d0da23acc604f82fa5938af9b342ef295688
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-11 23:30:25 +0530

    comments are updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter } from 'react-router' //Thisis used routing purpose
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <BrowserRouter>
-     <QueryClientProvider client={queryClient}>
+     <QueryClientProvider client={queryClient}>     
         <App />
       </QueryClientProvider>
     </BrowserRouter>
