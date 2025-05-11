ad296f68f6755204453595ab272dbbf476cdcc6a
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-11 23:30:35 +0530

    comments are updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router' //Thisis used routing purpose
+import { BrowserRouter } from 'react-router' //This is used routing purpose
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
