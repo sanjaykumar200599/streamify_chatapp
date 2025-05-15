86ece8da391ecd8d4feb7eed20bc77a511a56485
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-15 19:46:21 +0530

    main is updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router' //This is used for routing purpose
+import { BrowserRouter } from 'react-router' 
+//This is used for routing purpose
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
