0766203142335cf42eba62c71c5d275da13fe2ce
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-05-15 17:56:20 +0530

    server in backend is updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const app=express();
 const PORT=process.env.PORT
 
 app.use(cors({
-    origin:"https://localhost:5173",
+    origin:"http://localhost:5173",
     credentials: true,  // allow frontend to send cookies
 }));
 app.use(express.json());
